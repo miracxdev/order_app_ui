c01c777649f2db2409ec92aef27ed34474e5c05c
sonerxdev <sonerkaraevli@gmail.com>
2022-08-06 18:10:19 +0300

    Drawer menu added.

---
+++ lib/ui/notification_screen/view/notification_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/drawer.dart';
+
+class NotificationScreen extends StatefulWidget {
+  NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+    );
+  }
+}
